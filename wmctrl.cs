b62b5c2d543e4b4baee653c535af01c1455e3909
Caleb Biasco <18580431+cbiasco@users.noreply.github.com>
2025-04-03 21:26:57 +0200

    Fix formatting, fix failure cases

--- wmctrl.cs
+++ wmctrl.cs
@@ -10,7 +10,7 @@ public class wmctrl
 
     [DllImport("user32.dll")]
     static extern bool EnumThreadWindows(int dwThreadId, EnumThreadDelegate lpfn,
-					 IntPtr lParam);
+                     IntPtr lParam);
 
     [System.Runtime.InteropServices.DllImport("user32.dll", EntryPoint = "SendMessage", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
     public static extern bool SendMessage(IntPtr hWnd, uint Msg, int wParam, StringBuilder lParam);
@@ -21,7 +21,7 @@ public class wmctrl
     [DllImport("user32.dll")]
     public static extern void SwitchToThisWindow(IntPtr hWnd);
 
-    const int WM_GETTEXT       = 0x000D;
+    const int WM_GETTEXT = 0x000D;
     const int WM_GETTEXTLENGTH = 0x000E;
 
     // --------------------------------------------------------------------------------
@@ -29,57 +29,63 @@ public class wmctrl
     // --------------------------------------------------------------------------------
     static IEnumerable<IntPtr> EnumerateProcessWindowHandles(Process proc)
     {
-	var handles = new List<IntPtr>();
+        var handles = new List<IntPtr>();
 
-	foreach (ProcessThread thread in proc.Threads)
-	{
-	    EnumThreadWindows(thread.Id,
-			      (hWnd, lParam) => { handles.Add(hWnd); return true; }, IntPtr.Zero);
-	}
+        foreach (ProcessThread thread in proc.Threads)
+        {
+            EnumThreadWindows(thread.Id,
+                      (hWnd, lParam) => { handles.Add(hWnd); return true; }, IntPtr.Zero);
+        }
 
-	return handles;
+        return handles;
     }
 
     static IntPtr GetProcessWindowHandle(Process proc, string windowName)
     {
-	IntPtr handle = IntPtr.Zero;
-	StringBuilder title = new StringBuilder(1000);
+        IntPtr handle = IntPtr.Zero;
+        StringBuilder title = new StringBuilder(1000);
 
-	foreach (ProcessThread thread in proc.Threads)
-	{
-	    if (handle == IntPtr.Zero)
-	    {
-		EnumThreadWindows(thread.Id,
-				  (hWnd, lParam) => {
-				      SendMessage(hWnd, WM_GETTEXT, title.Capacity, title);
-				      if (title.ToString().Contains(windowName))
-				      {
-					  handle = hWnd;
-					  return false;
-				      }
-				      return true;
-				  }, IntPtr.Zero);
-	    }
-	}
+        foreach (ProcessThread thread in proc.Threads)
+        {
+            if (handle == IntPtr.Zero)
+            {
+                EnumThreadWindows(thread.Id,
+                          (hWnd, lParam) =>
+                          {
+                              SendMessage(hWnd, WM_GETTEXT, title.Capacity, title);
+                              if (title.ToString().Contains(windowName))
+                              {
+                                  handle = hWnd;
+                                  return false;
+                              }
+                              return true;
+                          }, IntPtr.Zero);
+            }
+        }
 
-	return handle;
+        return handle;
     }
 
     // --------------------------------------------------------------------------------
     // --- Switch to Window
     // --------------------------------------------------------------------------------
-    public static int SwitchToWindow(string procName){
+    public static int SwitchToWindow(string procName)
+    {
         // Getting window matching
         Process[] procs = Process.GetProcessesByName(procName);
         int nProcs = procs.Length;
-        if (nProcs < 1) {
+        if (nProcs < 1)
+        {
             Console.WriteLine("Error: No process found for name: {0}", procName);
-            return -1 ;
-        }else{
+            return -1;
+        }
+        else
+        {
             // We'll use the first window we found
-            Process proc=procs[0];
-            if (nProcs >1) {
-                Console.WriteLine("{0} processes found with name: {1}", nProcs,procName);
+            Process proc = procs[0];
+            if (nProcs > 1)
+            {
+                Console.WriteLine("{0} processes found with name: {1}", nProcs, procName);
                 Console.WriteLine("Using first process:");
                 Console.WriteLine("Process Name: {0} ID: {1} Title: {2}", proc.ProcessName, proc.Id, proc.MainWindowTitle);
             }
@@ -93,7 +99,8 @@ public class wmctrl
     // --------------------------------------------------------------------------------
     // --- List Processes info
     // --------------------------------------------------------------------------------
-    public static int ListProcesses(){
+    public static int ListProcesses()
+    {
         Process[] processlist = Process.GetProcesses();
         Console.WriteLine("ID: \t Name:\t Title:");
         Console.WriteLine("-------------------------------------------------");
@@ -101,7 +108,7 @@ public class wmctrl
         {
             if (!String.IsNullOrEmpty(proc.MainWindowTitle))
             {
-                Console.WriteLine("{0}\t {1}\t {2}", proc.Id,proc.ProcessName,  proc.MainWindowTitle);
+                Console.WriteLine("{0}\t {1}\t {2}", proc.Id, proc.ProcessName, proc.MainWindowTitle);
             }
         }
         return 0;
@@ -110,33 +117,39 @@ public class wmctrl
     // --------------------------------------------------------------------------------
     // --- List Windows info
     // --------------------------------------------------------------------------------
-    public static int ListWindows(string procName){
-	// Getting window matching
+    public static int ListWindows(string procName)
+    {
+        // Getting window matching
         Process[] procs = Process.GetProcessesByName(procName);
         int nProcs = procs.Length;
-        if (nProcs < 1) {
+        if (nProcs < 1)
+        {
             Console.WriteLine("Error: No process found for name: {0}", procName);
-            return -1 ;
-        }else{
-	    foreach (var proc in procs)
-	    {
+            return -1;
+        }
+        else
+        {
+            foreach (var proc in procs)
+            {
                 Console.WriteLine("Process Name: {0} ID: {1} Title: {2}", proc.ProcessName, proc.Id, proc.MainWindowTitle);
 
-		foreach (var handle in EnumerateProcessWindowHandles(proc))
-		{
-		    StringBuilder message = new StringBuilder(1000);
-		    SendMessage(handle, WM_GETTEXT, message.Capacity, message);
-		    if (string.IsNullOrWhiteSpace(message.ToString()))
-		    {
-			Console.WriteLine("---");
-		    }else{
-			Console.WriteLine(message);
-		    }
-		    Console.WriteLine("    {0}", handle);
-		}
+                foreach (var handle in EnumerateProcessWindowHandles(proc))
+                {
+                    StringBuilder message = new StringBuilder(1000);
+                    SendMessage(handle, WM_GETTEXT, message.Capacity, message);
+                    if (string.IsNullOrWhiteSpace(message.ToString()))
+                    {
+                        Console.WriteLine("---");
+                    }
+                    else
+                    {
+                        Console.WriteLine(message);
+                    }
+                    Console.WriteLine("    {0}", handle);
+                }
 
-		Console.WriteLine("");
-	    }
+                Console.WriteLine("");
+            }
             return 0;
         }
     }
@@ -144,22 +157,27 @@ public class wmctrl
     // --------------------------------------------------------------------------------
     // --- Get info for specific Window
     // --------------------------------------------------------------------------------
-    public static IntPtr GetWindowHandle(string procName, string windowName){
-	// Getting window matching
+    public static IntPtr GetWindowHandle(string procName, string windowName)
+    {
+        // Getting window matching
         Process[] procs = Process.GetProcessesByName(procName);
         int nProcs = procs.Length;
-        if (nProcs < 1) {
+        if (nProcs < 1)
+        {
             Console.Write("-3");
             return IntPtr.Zero;
-        }else{
-	    return GetProcessWindowHandle(procs[0], windowName);
+        }
+        else
+        {
+            return GetProcessWindowHandle(procs[0], windowName);
         }
     }
 
     // --------------------------------------------------------------------------------
     // --- Print command usage
     // --------------------------------------------------------------------------------
-    public static void print_usage(){
+    public static void print_usage()
+    {
         Console.WriteLine("");
         Console.WriteLine("usage: win_wmctrl [options] [args]");
         Console.WriteLine("");
@@ -178,85 +196,106 @@ public class wmctrl
     // --------------------------------------------------------------------------------
     public static int Main(string[] args)
     {
-        int status=0; // Return status for Main
+        int status = 0; // Return status for Main
 
         // --------------------------------------------------------------------------------
         // --- Parsing arguments
         // --------------------------------------------------------------------------------
-        int nArgs=args.Length;
-        if (nArgs==0){
+        int nArgs = args.Length;
+        if (nArgs == 0)
+        {
             Console.WriteLine("Error: insufficient command line arguments");
             print_usage();
             return 0;
         }
-        int i=0;
-        while (i<nArgs) {
-            string s=args[i];
-            switch(s){
+        int i = 0;
+        while (i < nArgs)
+        {
+            string s = args[i];
+            switch (s)
+            {
                 case "-h": // Help
                     print_usage();
-                    i=i+1;
+                    i = i + 1;
                     break;
                 case "-a": // Switch to Window via name
-                    if (i+1<nArgs) {
-			status=SwitchToWindow(args[i+1]);
-                        i=i+2;
-                    }else{
+                    if (i + 1 < nArgs)
+                    {
+                        status = SwitchToWindow(args[i + 1]);
+                        i = i + 2;
+                    }
+                    else
+                    {
                         Console.WriteLine("Error: command line option -a needs to be followed by a process name.");
-                        status=-1;
+                        status = -1;
+                        i++;
                     }
                     break;
-		case "-ia": // Switch to Window via window handle
-		case "-ai":
-		    if (i+1<nArgs) {
-			Int64 hWnd;
-			bool isNumeric = Int64.TryParse(args[i+1], out hWnd);
-			if (isNumeric)
-			{
-			    SwitchToThisWindow(new IntPtr(hWnd));
-			    status=0;
-			}else{
-			    Console.WriteLine("Error: command line option -i needs to be followed by a window handle number.");
-			    status=-1;
-			}
-                        i=i+2;
-                    }else{
+                case "-ia": // Switch to Window via window handle
+                case "-ai":
+                    if (i + 1 < nArgs)
+                    {
+                        Int64 hWnd;
+                        bool isNumeric = Int64.TryParse(args[i + 1], out hWnd);
+                        if (isNumeric)
+                        {
+                            SwitchToThisWindow(new IntPtr(hWnd));
+                            status = 0;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error: command line option -i needs to be followed by a window handle number.");
+                            status = -1;
+                        }
+                        i = i + 2;
+                    }
+                    else
+                    {
                         Console.WriteLine("Error: command line option -i needs to be followed by a window handle number.");
-                        status=-1;
+                        status = -1;
+                        i++;
                     }
-		    break;
-	        case "-p":
-		    status=0; // The status will never be non-zero for -p, because the console output is expected to be used by the caller.
-		    if (i+2<nArgs) {
-			IntPtr handle = GetWindowHandle(args[i+1], args[i+2]);
-			if (handle == IntPtr.Zero)
-			{
-			    Console.Write("-2");
-			}
-			else
-			{
-			    Console.Write("{0}", handle);
-			}
-                        i=i+3;
-                    }else{
+                    break;
+                case "-p":
+                    status = 0; // The status will never be non-zero for -p, because the console output is expected to be used by the caller.
+                    if (i + 2 < nArgs)
+                    {
+                        IntPtr handle = GetWindowHandle(args[i + 1], args[i + 2]);
+                        if (handle == IntPtr.Zero)
+                        {
+                            Console.Write("-2");
+                        }
+                        else
+                        {
+                            Console.Write("{0}", handle);
+                        }
+                        i = i + 3;
+                    }
+                    else
+                    {
                         Console.Write("-1");
+                        i++;
                     }
-		    break;
+                    break;
                 case "-l": // List Processes/Windows
-		    if (i+1<nArgs) {
-			status=ListWindows(args[i+1]);
-			i=i+2;
-		    }else{
-			status=ListProcesses();
-		    }
+                    if (i + 1 < nArgs)
+                    {
+                        status = ListWindows(args[i + 1]);
+                        i = i + 2;
+                    }
+                    else
+                    {
+                        status = ListProcesses();
+                    }
                     i++;
                     break;
                 default:
-                    Console.WriteLine("Skipped argument: "+ args[i]);
+                    Console.WriteLine("\nSkipped argument: " + args[i]);
                     i++;
                     break;
             }
-            if (status!=0) {
+            if (status != 0)
+            {
                 // If an error occured, print usage and exit
                 print_usage();
                 return status;
